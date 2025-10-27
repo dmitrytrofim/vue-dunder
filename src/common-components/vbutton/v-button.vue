471c36b504d7e0531697116acdb14a08e67fafd1
dmitrytrofim <d.trofimov@jetmail.cc>
2025-10-27 09:05:11 +0300

    add scss + button

---
+++ src/common-components/vbutton/v-button.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="tag" :href="props.isLink ? url : null"
+    ><slot></slot
+  ></component>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+type LinkProps =
+  | { isLink: true; url: string }
+  | { isLink?: false; url?: never };
+
+const props = defineProps<LinkProps>();
+
+const tag = computed(() => (props.isLink ? 'a' : 'button'));
+</script>
+
+<style src="./v-button.scss" scoped></style>
---
+++ src/components/counter/counter.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="test">test + 1</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style src="./counter.scss" scoped></style>
